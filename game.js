56a3679983f9af2741157940b1bb729923d6b63f
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-03-24 19:54:32 -0700

    Add background sprite

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
+//Global PIXI.js app variable
 var app;
+var game = {};
 
 $(()=>{
   //When sign in button is clicked
@@ -11,10 +13,13 @@ $(()=>{
       createGame();
     }
   });
-})
+});
 
 //Called after signin
 function createGame(){
+    //TODO connect to server and make a socket
+    $("#signin").append("<p>Logging in...</p>");
+
   //Create application
   app = new PIXI.Application({
     "width":640,
@@ -24,10 +29,38 @@ function createGame(){
   //Add child to DOM
   document.body.appendChild(app.view);
 
-  //TODO connect to server and make a socket
+  //Load everything
+  load();
 }
 
 //Will handle all loading
 function load(){
+  app.loader.add('bg1', 'resources/images/bg1.png');
+  console.log("load() has been reached");
+  app.loader.load(setup);
+}
 
+function setup(){
+  console.log("setup() has been reached");
+  //Make background sprite
+  game.bg = new PIXI.extras.TilingSprite(
+    app.loader.resources["bg1"].texture,
+    640, 480);
+
+  //Add new sprites to stage
+  app.stage.addChild(game.bg)
+  //This specifies the loop
+  app.ticker.add(()=>{
+    //Call the current game state loop and render it
+    game.state();
+    app.render();
+  });
+  app.start();
+}
+
+//Holds the game's current state
+game.state = play;
+
+function play(){
+  //TODO do stuff
 }
